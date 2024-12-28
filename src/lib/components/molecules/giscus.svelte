6f9bd61dd81981a0e6882a2aa9b6ebf3bb11e9f7
Rasel Shikdar <imrasel597@gmail.com>
2024-12-29 02:36:41 +0600

    Create giscus.svelte

---
+++ src/lib/components/molecules/giscus.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Giscus from '@giscus/svelte';
+</script>
+
+<div id="comments">
+  <Giscus
+    id="comments"
+    repo="raselshikdar/rsinfo"
+    repoId="R_kgDOMEUUZA"
+    category="General"
+    categoryId="DIC_kwDOMEUUZM4ClhwM"
+    mapping="pathname"
+    strict="0"
+    reactionsEnabled="1"
+    emitMetadata="0"
+    inputPosition="top"
+    theme="light"
+    lang="en"
+    loading="lazy"
+  />
+</div>
+
+<style>
+  #comments {
+    width: 100%;
+    margin: 0 auto;
+    padding-top: 20px;
+  }
+</style>

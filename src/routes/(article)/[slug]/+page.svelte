45860b7277b238b886b1ec3638040040eaf13e6b
Rasel Shikdar <imrasel597@gmail.com>
2024-12-28 16:21:14 +0600

    Update +page.svelte

--- src/routes/(article)/[slug]/+page.svelte
+++ src/routes/(article)/[slug]/+page.svelte
@@ -6,7 +6,7 @@
     import Giscus from '@giscus/svelte';
     import { page } from '$app/stores';
     import { theme } from '$lib/stores/theme';
-    import { languageTag } from '@inlang/paraglide-js/dist/runtime';
+    import { languageTag } from '@inlang/paraglide-js';
     
     export let data: { post: BlogPost };
     $: ({ post } = data);

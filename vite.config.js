f6513a05c7913d29ef6de27e213a94e9fe1fbb67
Rasel Shikdar <imrasel597@gmail.com>
2024-12-28 16:16:37 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			$routes: path.resolve('./src/routes'),
-			$cms: path.resolve('./cms')
-		}
-	}
+    plugins: [sveltekit()],
+    resolve: {
+        alias: {
+            $routes: path.resolve('./src/routes'),
+            $cms: path.resolve('./cms'),
+            '@inlang/paraglide-js': path.resolve('./node_modules/@inlang/paraglide-js/dist')
+        }
+    }
 };
 
 export default config;

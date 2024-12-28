2fa5db9dc85d979d118a79f20b3b88b37aea8469
Rasel Shikdar <imrasel597@gmail.com>
2024-12-28 16:32:29 +0600

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,19 +5,19 @@ const extensions = ['.svelte', '.md'];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter(),
-        prerender: {
-            handleHttpError: ({ path }) => {
-                if (path.indexOf('.avif') || path.indexOf('.webp') || path.indexOf('.png')) {
-                    // Ignore 404 errors for images (as they're likely not optimized yet at build time)
-                    return;
-                }
-            }
-        }
-    },
-    preprocess: [vitePreprocess()],
-    extensions: extensions
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path }) => {
+				if (path.indexOf('.avif') || path.indexOf('.webp') || path.indexOf('.png')) {
+					// Ignore 404 errors for images (as they're likely not optimized yet at build time)
+					return;
+				}
+			}
+		}
+	},
+	preprocess: [vitePreprocess()],
+	extensions: extensions
 };
 
 export default config;
